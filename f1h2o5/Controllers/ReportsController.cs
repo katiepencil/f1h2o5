0748b8b1c29085d2f3b7136d61aa47b7a6a70508
Katie Thompson <katiepencil@yahoo.com>
2016-12-19 16:58:13 -0600

    added linq queries!

--- f1h2o5/Controllers/ReportsController.cs
+++ f1h2o5/Controllers/ReportsController.cs
@@ -20,26 +20,34 @@ namespace f1h2o5.Controllers
         {
             return View();
         }
-
-        // GET:All drivers who raced in a given year.
-        // This is not working yet.
-        [Authorize(Roles = "Admin")]
-        public ActionResult AllDriversForYearX(string RaceYear)
+        
+        // ok, start by first name of all drivers!
+         [Authorize(Roles = "Admin")]
+        public ActionResult FirstNamesOfDrivers()
         {
-            var yearModel = from d in db.DriverRunsRaces.Include("Race")
-                            select d.Race.RaceYear;
-            return View(yearModel);
+            var result = from d in db.Drivers
+                         select d.FirstName.ToString();
+            return View(result);
         }
 
         // GET:All race results for a given driver.
-        // ok, start by drivers with first name starting with letter 'p'!
         // logic not correct yet (I seem to be asking for a boolean result)
         // but LINQ is working!
         [Authorize(Roles = "Admin")]
         public ActionResult AllRaceResultsForDriverX()
         {
             var result = from d in db.Drivers
-            select d.FirstName.StartsWith("P");
+            select d.LastName.ToString();
+            return View(result);
+        } 
+        
+        // GET:All drivers who raced in a given year.
+        // This is not working yet.
+        [Authorize(Roles = "Admin")]
+        public ActionResult AllDriversForYearX()
+        {
+            var result = from d in db.Teams
+                         select d.Name.ToString();
             return View(result);
         }
 
@@ -47,8 +55,9 @@ namespace f1h2o5.Controllers
         [Authorize(Roles = "Admin")]
         public ActionResult AllTeamsForYearX()
         {
-
-            return View();
+            var result = from d in db.Drivers
+                         select d.City.ToString();
+            return View(result);
         }
     }
 }
